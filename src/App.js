c8c56bf6959c3a7bf496a63f5002d87393cd86b4
Shamail <shamailabbas61@gmail.com>
2022-01-22 14:11:44 +0500

    updated react router dom to v6

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import React from 'react'
-import Main from '../src/components/Main'
-import '../src/css/main.css'
-import { Switch, Route, Redirect } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Aboutpage from './components/Aboutpage'
-import Workpage from './components/Workpage'
-import Contactpage from './components/Contactpage'
-import "./App.css"
+import React from "react";
+import Main from "../src/components/Main";
+import "../src/css/main.css";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Aboutpage from "./components/Aboutpage";
+import Workpage from "./components/Workpage";
+import Contactpage from "./components/Contactpage";
+import "./App.css";
+
 const App = () => {
   return (
     <>
-      <div className='container'>
+      <div className="container">
         <Navbar></Navbar>
-        <Switch>
-          <Route exact path='/' component={Main} />
-          <Route exact path='/workpage' component={Workpage} />
-          <Route exact path='/contactpage' component={Contactpage} />
+        <Routes>
+          <Route exact path="/" element={<Main />} />
+          <Route exact path="/workpage" element={<Workpage />} />
+          <Route exact path="/contactpage" element={<Contactpage />} />
 
-          <Route exact path='/aboutpage' component={Aboutpage} />
-          <Redirect to='/' />
-        </Switch>
+          <Route exact path="/aboutpage" element={<Aboutpage />} />
+          <Route path="*" element={<Navigate replace to="/" />} />
+        </Routes>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
